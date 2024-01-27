47721fa6d338ec82d4b8c030dba864333386dbd2
maritocuate <mario300@gmail.com>
2024-01-27 11:06:01 -0300

    chore: styles setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 
 export default function Home() {
-  return <MaxWidthWrapper>MBank</MaxWidthWrapper>
+  return (
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="index-container sm:px-6 lg:px-8">
+        <div className="index-container--logo sm:mx-auto sm:w-full sm:max-w-md">
+          <Image
+            alt="Logo"
+            height="48"
+            width="48"
+            src="/images/logo.svg"
+            priority
+          />
+          <h2 className="index-container--title">Sign in</h2>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  )
 }
