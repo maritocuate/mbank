9ebd7e828eb34562c6d0069eda7db84a2de7e1e4
maritocuate <mario300@gmail.com>
2024-01-29 01:07:43 -0300

    chore: popup ui component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import MainPanel from './components/MainPanel'
-import Popup from './components/Popup'
 
 export default function Page() {
   return (
